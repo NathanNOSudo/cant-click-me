89f743fb4afd262ec0b0008e393d2180267de81d
Nathan Orris <nathanorris@protonmail.com>
2023-06-18 12:25:12 -0400

    add dynamic text and contain text on button shapes

--- script.js
+++ script.js
@@ -30,6 +30,50 @@ function moveButton(e) {
     changeShape();
 }
 
+// We need the button text to change as well, we want it to say 'Try Again'
+function changeButtonText() {
+    if (usernameError.textContent || emailError.textContent || passwordError.textContent || !username.value || !email.value || !password.value) {
+        btn.textContent = 'Try Again';
+    }
+}
+
+function revertButtonText() {
+    btn.textContent = 'Submit';
+}
+
+// Change button text back to 'Submit' when the form is correctly filled out
+form.addEventListener('input', function() {
+    clearErrors();
+
+    if (!username.value) {
+        showError(username, 'Username is required');
+    } else if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+        showError(email, 'Enter a valid email');
+    } else if (!password.value) {
+        showError(password, 'Password is required');
+    } else {
+        revertButtonText();
+    }
+});
+
+btn.addEventListener('mouseover', function(e) {
+    if (usernameError.textContent || emailError.textContent || passwordError.textContent || !username.value || !email.value || !password.value) {
+        moveButton(e);
+        changeButtonText();
+    }
+});
+
+// Change button text back to 'Submit' when the form is correctly filled out
+btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (usernameError.textContent || emailError.textContent || passwordError.textContent || !username.value || !email.value || !password.value) {
+        moveButton(e);
+        changeButtonText();
+    } else {
+        alert('Form submitted');
+    }
+});
+
 function changeShape() {
     let randomShape = shapes[Math.floor(Math.random() * shapes.length)];
     btn.className = ''; // Clear previous shape
